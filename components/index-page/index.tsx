3584986af09b9c747933e25a553062ebb7ee9edf
Jerry Fan <huanyueyaoqin@qq.com>
2024-12-09 22:01:57 +0800

    chore: 兼容微信内置浏览器

--- components/index-page/index.tsx
+++ components/index-page/index.tsx
@@ -7,18 +7,17 @@ import { Contributors } from "../contributors";
 import { Testimonials } from "../testimonials";
 import Footer from "../footer";
 import { useTranslation } from "../../hooks/useTranslation";
-import { useRouter } from 'next/router';
-import Image from 'next/image';
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 const IndexPage: React.FC = () => {
   const { t } = useTranslation();
-  const { basePath } = useRouter();
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.hero}>
-          <h1 className={styles.title}>{t("common.title")}</h1>
+          <div className={styles.title}>{t("common.title")}</div>
 
           <p className={styles.subtitle}>{t("common.subtitle")}</p>
           <div className={styles.buttonGroup}>
